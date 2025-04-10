782e6ca07dba9481f659ffb8ec42e88b2cda5041
SakshiAmuze <sakshi@amuze.in>
2025-04-10 20:52:40 +0530

    css  changes

--- src/Components/Corporate/Corporate_Home/Corporate_Home.js
+++ src/Components/Corporate/Corporate_Home/Corporate_Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import Corporate_Header from '../Corporate_Header/Corporate_Header'
+import './Corporate_Home.css'
 function Corporate_Home() {
   return (
-    <div>Corporate_Home</div>
-  )
+
+    <>
+    <Corporate_Header/>
+   <div className="container custom-c mt-5">
+      <div className="row ">
+        <div className="col-md-4  p-3 mt-3"><Leftsidebar></Leftsidebar></div>
+        <div className="col-md-4  p-3 mt-3"><Feed></Feed></div>
+        <div className="col-md-4  p-3 mt-3"><Rightsidebar></Rightsidebar></div>
+      </div>
+    </div>
+    </>
+
+    )
 }
 
 export default Corporate_Home
